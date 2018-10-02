2e0dac241469003491a055ea3f519c45d2ddaa18
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-10-02 11:33:10 +1000

    Create PhotoController with all the methods (create, delete, display) and generate the view automatically

--- Day-1/PhotoSharingApplication/PhotoSharingApplication/Models/PhotoSharingContext.cs
+++ Day-1/PhotoSharingApplication/PhotoSharingApplication/Models/PhotoSharingContext.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Data.Entity;
 
 namespace PhotoSharingApplication.Models
-{
+{ 
     public class PhotoSharingContext : DbContext
     {
         public DbSet<Photo> Photos { get; set; }
