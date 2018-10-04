56bebca791161c2315de3d8bb5206f0a6f0bab7c
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-10-04 14:19:32 +1000

    Lab Solution - Module 13: Implementing Web APIs in ASP.NET MVC 5 Web Applications

---
+++ Day-4/PhotoSharingApplication_13_solution/PhotoSharingApplication/App_Start/WebApiConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace PhotoSharingApplication
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "PhotoApi",
+                routeTemplate: "api/photos/{id}",
+                defaults: new { controller = "PhotoApi", action = "GetPhotoById" },
+                constraints: new { id = "[0-9]+" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "PhotoTitleApi",
+                routeTemplate: "api/photos/{title}",
+                defaults: new { controller = "PhotoApi", action = "GetPhotoByTitle" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "PhotosApi",
+                routeTemplate: "api/photos",
+                defaults: new { controller = "PhotoApi", action = "GetAllPhotos" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var json = config.Formatters.JsonFormatter;
+
+            /* 
+             * Set the json.SerializerSettings.PreserveReferencesHandling property to 
+             * Newtonsoft.Json.PreserveReferencesHandling.Objects.
+             */
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
