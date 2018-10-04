b80e8f3ca529eeab36a3ce02558387a38f1c2b31
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-10-04 10:45:42 +1000

    Lab Solution - Module 11: Controlling Access to ASP.NET MVC 5 Web Applications

---
+++ Day-4/PhotoSharingApplication_11_solution/PhotoSharingApplication/Models/AccountModelClasses.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace PhotoSharingApplication.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(): base("PhotoAppServices")
+        {
+
+        }
+    }
+
+    public class Login
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class Register
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LocalPassword
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
